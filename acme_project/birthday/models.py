c68d7e4285c10826984c879e4cfabe3be90bd025
Вероника Попова <nikapopova098@gmail.com>
2025-12-04 14:56:55 +0400

    project with paginator

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,11 @@
+# birthday/models.py
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
+    )
+    birthday = models.DateField('Дата рождения')
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,3 +1,4 @@
+# birthday/urls.py
 from django.urls import path
 
 from . import views
@@ -6,4 +7,8 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
+    # Новый маршрут.
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
 ]
