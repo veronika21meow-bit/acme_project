759d6df282de5285fffdff867189f675c63e624f
Вероника Попова <nikapopova098@gmail.com>
2025-12-10 14:35:36 +0400

    registration

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,12 +1,28 @@
 # acme_project/urls.py
 # Импортируем настройки проекта.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+
+from users.forms import CustomUserCreationForm
+
+
 
 urlpatterns = [
+    path(
+        'auth/registration/', 
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=CustomUserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
